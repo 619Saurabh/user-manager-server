38055afbf13487b30244c8da20e195ed379b2d87
Saurabh Singh <saurabh26sep2000@gmail.com>
2025-07-03 00:54:28 +0530

    Update / in CORS with Netlify site origin

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 // app.use(cors());
-app.use(cors({ origin: 'https://bishtusermanager.netlify.app/' }));
+app.use(cors({ origin: 'https://bishtusermanager.netlify.app' }));
 app.use(express.json());
 
 let users = [
