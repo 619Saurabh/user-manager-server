bf81a8944e971b63228af180bb5fd3305aa9c468
Saurabh Singh <saurabh26sep2000@gmail.com>
2025-07-03 01:31:47 +0530

    undo all changes

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-// const PORT = 5000;
-const PORT = process.env.PORT || 5000
+const PORT = 5000;
 
-// app.use(cors());
-app.use(cors({ origin: 'https://bishtusermanager.netlify.app' }));
+app.use(cors());
 app.use(express.json());
 
 let users = [
@@ -29,7 +27,6 @@ app.post('/api/users', (req, res) => {
   users.push(newUser);
   res.status(201).json(newUser);
 });
-
 
 // Delete a user
 app.delete('/api/users/:id', (req, res) => {
